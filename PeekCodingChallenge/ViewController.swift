6b4eb62e73b7ada82518cb1df19b55c9daec3bc4
cagaray <cgaray@gmail.com>
2016-05-13 21:05:47 -0700

    Base Project

---
+++ PeekCodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeekCodingChallenge
+//
+//  Created by Cristián Garay on 5/13/16.
+//  Copyright © 2016 Cristian Garay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
